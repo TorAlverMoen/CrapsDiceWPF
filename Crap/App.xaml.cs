49912342247b4736cf15cab6181a45dca50b0612
Tor Alver Moen <tor.alver.moen@gmail.com>
2024-06-24 10:53:33 +0200

    Added a splash screen
    #jira CD2-11

--- Crap/App.xaml.cs
+++ Crap/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Crap
 {
@@ -9,6 +7,15 @@ namespace Crap
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            SplashScreen splash = new SplashScreen("img/CrapSplash.png");
+            splash.Show(autoClose: false);
+            Thread.Sleep(4000);
+            splash.Close(TimeSpan.FromMilliseconds(500));
+        }
     }
 
 }
